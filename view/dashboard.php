84236890db3869620dcb7a3ce4fdf13a5b52f583
MD. ROKONUZZAMAN <rokonkhan1819@gmail.com>
2025-05-23 20:26:41 +0600

    dashboard header file

--- view/dashboard.php
+++ view/dashboard.php
@@ -11,35 +11,9 @@ require_once '../controler/dashboard_controller.php';
     <link rel="stylesheet" href="../assets/css/dashboard.css">
 </head>
 <body>
-    <header>
-        <div class="logo">Blogging</div>
-        <nav>
-            <a href="#">Our story</a>
-            <a href="Newsltrsignup.html">Membership</a>
-            <a href="../Blogging-Platform/view/login.php">Write</a>
-            <div class="notification">
-                <span class="bell">&#128276;</span>
-                <span class="badge">3</span>
-            </div>
-            <div class="profile">
-                <img src="../assets/img/istockphoto-1053936212-1024x1024.jpg" alt="Profile" class="profile-img">
-                <span class="username" onclick="toggleMenu()">
-                    <?php
-                    if (isset($_SESSION['username'])) {
-                        echo $_SESSION['username'];
-                    } else {
-                        echo 'Profile';
-                    }
-                    ?>
-                </span>
-                <div class="dropdown-menu" id="profileMenu">
-                    <a href="../view/profile.php">Update Profile</a>
-                    <a href="../view/adminpage.php">Admin Page</a>
-                    <a href="../controler/logout.php">Logout</a>
-                </div>
-            </div>
-        </nav>
-    </header>
+    <?php
+  include '../view/header.php';
+    ?>
     <div class="container">
         <div class="header">
             <h2>Recent Articles</h2>
