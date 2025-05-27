90c54c62531f697ce78b86f5ea8d49fb36c158f6
MD. ROKONUZZAMAN <rokonkhan1819@gmail.com>
2025-05-28 00:57:43 +0600

    alluser function

--- controler/admin_usercontroler.php
+++ controler/admin_usercontroler.php
@@ -7,17 +7,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['delete_id'])) {
     deleteUser($deleteId);
 }
 
-// Fetch all users from the database
-function getAllUsers() {
-    $con = getDatabaseConnection();
-    $sql = "SELECT id, username, email, role FROM users";
-    $result = mysqli_query($con, $sql);
-    $users = [];
-    while ($row = mysqli_fetch_assoc($result)) {
-        $users[] = $row;
-    }
-    return $users;
-}
 
 $users = getAllUsers();
 ?>
